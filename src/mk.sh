58a156f77476db76c0982391d3b36ed09b42d92d
mirnshi <mirnshi@285c826d-31ef-41a9-a5b6-d2a4949dcdf2>
2013-09-20 08:12:58 +0000

    1. using the script 'mk.sh' to compile vpcs
    git-svn-id: http://svn.code.sf.net/p/vpcs/code/trunk@80 285c826d-31ef-41a9-a5b6-d2a4949dcdf2

---
+++ src/mk.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+os=`uname`
+
+if [ $# -eq 0 ]; then
+    arch=`uname -p`
+fi
+
+if [ $# -eq 1 ]; then
+    if [ $1 = "clean" ]; then
+        MKOPT=clean
+        arch=`uname -p`
+    else     
+        arch=$1 
+    fi
+fi
+
+if [ $# -eq 2 ]; then
+    if [ $1 = "clean" ]; then
+        MKOPT=clean
+        arch=$2
+    fi
+    if [ $2 = "clean" ]; then
+        MKOPT=clean
+        arch=$1
+    fi
+fi
+
+if [ "x$arch" = "x" ]; then
+    echo $0" [i386 | amd64 | 32 | 64] [clean]"
+    exit 1
+fi
+
+CCOPT=" "
+
+if [ $arch = "i386" -o $arch = "32" ]; then
+    CCOPT="-m32"
+fi
+
+if [ $arch = "amd64" -o $arch = "x86_64" -o $arch = "64" ]; then
+    CCOPT="-m64"
+fi
+
+if [ $os = "Darwin" ]; then
+    CCOPT=" "
+fi
+
+export CCOPT
+case ${os} in
+    FreeBSD)
+    	make -f Makefile.fbsd ${MKOPT}
+    	;;
+    Linux)
+        make -f Makefile.linux ${MKOPT}
+        ;;
+    CYGWIN*)
+        make -f Makefile.cygwin ${MKOPT}
+        ;;
+    Darwin)
+        make -f Makefile.osx ${MKOPT}
+        ;;
+    *)
+    	echo "Not support"
+    	;;
+esac
