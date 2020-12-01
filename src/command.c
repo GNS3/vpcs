b94883505028c98b1d59ff32bd29fa2096bb87eb
grossmj <grossmj@gns3.net>
2020-12-01 22:04:28 +1030

    Revert "Save the pcname if 12 characters are differents."
    This reverts commit 6c6f932d

--- src/command.c
+++ src/command.c
@@ -2046,8 +2046,8 @@ int run_save(int argc, char **argv)
 		if (num_pths > 1)
 			fprintf(fp, "%d\n", i + 1);
 		sprintf(buf, "VPCS[%d]", i + 1);
-		if (strncmp(vpc[i].xname, buf, MAX_NAMES_LEN))
-		    fprintf(fp, "set pcname %s\n", vpc[i].xname);
+		//if (strncmp(vpc[i].xname, buf, 6))
+		fprintf(fp, "set pcname %s\n", vpc[i].xname);
 
 		if (num_pths > 1) {
 			if (vpc[i].lport != (20000 + i))
